a35923c27d5a09f4705ec7349db9e1f3be3efd84
venesia-6 <venesia.arisaputri@gmail.com>
2026-05-02 17:21:55 +0700

    Fix nltk

--- Home.py
+++ Home.py
@@ -2,6 +2,8 @@ import streamlit as st
 import nltk
 
 nltk.download('stopwords')
+nltk.download('punkt')
+nltk.download('punkt_tab')
 nltk.download('averaged_perceptron_tagger_eng')
 nltk.download('wordnet')
 
